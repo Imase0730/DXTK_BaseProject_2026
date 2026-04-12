bfc3d6e5121d9f333b08be67a0d23095c61d6cc6
imase0730@gmail.com <imase0730@gmail.com>
2026-04-13 04:23:16 +0900

    プロジェクトファイルを追加。

---
+++ DXTK_BaseProject_2026/Scene/BaseScene/BaseScene.cpp
@@ -0,0 +1,46 @@
+﻿//--------------------------------------------------------------------------------------
+// File: BaseScene.cpp
+//
+// 新規シーン作成時の元にするファイル
+//
+// Date: 2026.4.13
+// Author: Hideyasu Imase
+//--------------------------------------------------------------------------------------
+#include "pch.h"
+#include "BaseScene.h"
+#include "../ShootingGameScene/ShootingGameScene.h"
+
+using namespace DirectX;
+
+// 更新
+void BaseScene::Update(Imase::ISceneController<SceneId>& sceneController, GameContext& gameContext)
+{
+	float elapsedTime = float(gameContext.timer.GetElapsedSeconds());
+
+	elapsedTime;
+
+	Keyboard::KeyboardStateTracker& tracker = gameContext.keyboardTracker;
+
+	// スペースキーが押された
+	if (tracker.pressed.Space)
+	{
+		// 次のシーンへ
+		sceneController.RequestSwitch(SceneId::ShootingGameScene);
+	}
+
+	auto& debugRenderer = gameContext.debugRenderer;
+
+	debugRenderer.DrawText({ 0.0f, 0.0f }, L"BaseScene");
+}
+
+// 描画
+void BaseScene::Render(GameContext& gameContext)
+{
+	auto context = gameContext.deviceResources.GetD3DDeviceContext();
+	auto& states = gameContext.commonStates;
+}
+
+// シーン切り替え時に呼び出される関数
+void BaseScene::OnEnter(GameContext& gameContext)
+{
+}
