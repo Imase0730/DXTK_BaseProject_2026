cf42d2d030dc2e615da59fc7847cacfba455e904
imase <imase0730@gmail.com>
2026-04-13 09:28:51 +0900

    3DPG応用のベースプロジェクト

--- DXTK_BaseProject_2026/Scene/BaseScene/BaseScene.cpp
+++ DXTK_BaseProject_2026/Scene/BaseScene/BaseScene.cpp
@@ -8,27 +8,13 @@
 //--------------------------------------------------------------------------------------
 #include "pch.h"
 #include "BaseScene.h"
-#include "../ShootingGameScene/ShootingGameScene.h"
 
 using namespace DirectX;
 
 // 更新
 void BaseScene::Update(Imase::ISceneController<SceneId>& sceneController, GameContext& gameContext)
 {
-	float elapsedTime = float(gameContext.timer.GetElapsedSeconds());
-
-	elapsedTime;
-
-	Keyboard::KeyboardStateTracker& tracker = gameContext.keyboardTracker;
-
-	// スペースキーが押された
-	if (tracker.pressed.Space)
-	{
-		// 次のシーンへ
-		sceneController.RequestSwitch(SceneId::ShootingGameScene);
-	}
-
-	auto& debugRenderer = gameContext.debugRenderer;
+	Imase::DebugRenderer& debugRenderer = gameContext.debugRenderer;
 
 	debugRenderer.DrawText({ 0.0f, 0.0f }, L"BaseScene");
 }
